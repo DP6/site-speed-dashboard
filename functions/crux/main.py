a2917b821eebb8fda1a87fc66fc03bf0d1ec09d4
Gian Gabriel Panacioni <66138840+GianGP@users.noreply.github.com>
2022-01-25 18:11:53 -0300

    fix: :bug: Ajuste execução crux CF
    Adição de return na main, possibilitando execução da cloud function por http

--- functions/crux/main.py
+++ functions/crux/main.py
@@ -20,6 +20,7 @@ def main(event,context):
 
     if(crux.check_last_month()):
         logging.info("Previously updated CRUX table.")
+        return "Finalizado!"
         
     else:
          
@@ -29,3 +30,4 @@ def main(event,context):
                 logging.info("CRUX table updated successfully.") 
         else:
             logging.info("CRUX table not available.")
+        return "Em execução!"
