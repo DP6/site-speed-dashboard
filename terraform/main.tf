82eae8be6bdae3ca0449801cfb2bd943ce34edbc
Gian Gabriel Panacioni <66138840+GianGP@users.noreply.github.com>
2022-02-03 16:31:28 -0300

    fix: :bug: Ajuste nomenclatura e funcionalidade terraform
    Adição de seleção de países para a análise do crux no terraform, renomeação de alguns recursos a serem criados e ajuste na função do crux para adaptar à CF

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 # Cloud Storage Configuration
 ######################################################
 resource "google_storage_bucket" "my_storage" {
-  name          = local.final_bucket_name
+  name          = local.bucket_name
   location      = var.location
   force_destroy = true
 
@@ -12,7 +12,7 @@ resource "google_storage_bucket" "my_storage" {
 }
 
 resource "local_file" "config_json" { 
-  content = templatefile("./config_template.tftpl", { dataset = local.project_prefix }) 
+  content = templatefile("./config_template.tftpl", { dataset = local.project_prefix, lista = local.countries_ids }) 
   filename = "./files-copy-to-gcs/config/config.json"
 }
 
@@ -22,7 +22,7 @@ resource "null_resource" "cf_code_zip" {
   }
 
   provisioner "local-exec" {
-    command = "sh scripts/create_and_send_zips ${var.project_version} ${local.final_bucket_name}"
+    command = "sh scripts/create_and_send_zips.sh ${var.project_version} ${local.bucket_name}"
   }
 
   depends_on = [google_storage_bucket.my_storage]
@@ -34,7 +34,7 @@ resource "null_resource" "cf_code_zip" {
 #dataset
 resource "google_bigquery_dataset" "site_speed_dashboard" {
   location                   = var.location
-  dataset_id                 = local.final_dataset_id
+  dataset_id                 = local.dataset_id
   description                = "Site Speed Dashboard Dataset"
   delete_contents_on_destroy = true
 
@@ -44,7 +44,7 @@ resource "google_bigquery_dataset" "site_speed_dashboard" {
 }
 
 resource "google_bigquery_table" "psi_metrics_results" {
-  dataset_id          = local.final_dataset_id
+  dataset_id          = local.dataset_id
   table_id            = local.bq_table_psi_metrics_results
   description         = "Table with PageSpeed Insights collected metrics"
   schema              = file("bigquery/psi_metrics_results.json")
@@ -64,7 +64,7 @@ resource "google_bigquery_table" "psi_metrics_results" {
 }
 
 resource "google_bigquery_table" "crux_table" {
-  dataset_id          = local.final_dataset_id
+  dataset_id          = local.dataset_id
   table_id            = local.bq_table_crux_table
   description         = "Table with CrUX collected metrics"
   schema              = file("bigquery/crux_table.json")
@@ -84,7 +84,7 @@ resource "google_bigquery_table" "crux_table" {
 }
 
 resource "google_bigquery_table" "psi_suggestions_results" {
-  dataset_id          = local.final_dataset_id
+  dataset_id          = local.dataset_id
   table_id            = local.bq_table_psi_suggestions_results
   description         = "Table with PageSpeed Insights collected Suggestions"
   schema              = file("bigquery/psi_suggestions_results.json")
@@ -110,7 +110,7 @@ resource "google_bigquery_table" "psi_suggestions_results" {
 ##################################
 resource "google_cloudfunctions_function" "psi" {
   project               = var.project_id
-  name                  = local.cf_name
+  name                  = local.cf_name_psi
   description           = "Execution of PageSpeed Insights API to collect performance metrics"
   runtime               = "nodejs14"
   service_account_email = var.service_account_email
@@ -118,13 +118,13 @@ resource "google_cloudfunctions_function" "psi" {
   available_memory_mb   = 512
   timeout               = 540
   source_archive_bucket = google_storage_bucket.my_storage.name
-  source_archive_object = "${local.gcs_bucket_folder_name}/${var.project_version}.zip"
+  source_archive_object = "${local.gcs_bucket_folder_name}/psi.zip"
   trigger_http          = true
-  entry_point           = local.cf_entry_point
+  entry_point           = local.cf_entry_point_psi
   environment_variables = {
-    PROJECT_BUCKET_GCS = local.final_bucket_name
+    PROJECT_BUCKET_GCS = local.bucket_name
     PSI_KEY = var.psi_key
-    BQ_DATASET_ID = local.final_dataset_id
+    BQ_DATASET_ID = local.dataset_id
   }
   depends_on = [null_resource.cf_code_zip]
 }
@@ -140,9 +140,9 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   member = "allUsers"
 }
 
-resource "google_cloudfunctions_function" "crux_data" {
+resource "google_cloudfunctions_function" "crux" {
   project               = var.project_id
-  name                  = "crux_data"
+  name                  = local.cf_name_crux
   description           = "Execution of CrUX API to collect performance metrics"
   runtime               = "python39"
   service_account_email = var.service_account_email
@@ -150,12 +150,12 @@ resource "google_cloudfunctions_function" "crux_data" {
   available_memory_mb   = 256
   timeout               = 120
   source_archive_bucket = google_storage_bucket.my_storage.name
-  source_archive_object = "${local.gcs_bucket_folder_name}/crux_data.zip"
+  source_archive_object = "${local.gcs_bucket_folder_name}/crux.zip"
   trigger_http          = true
-  entry_point           = "main"
+  entry_point           = local.cf_entry_point_crux
   environment_variables = {
-    PROJECT_BUCKET_GCS = local.final_bucket_name
-    PROJECT_DATASET_BQ = local.final_dataset_id
+    PROJECT_BUCKET_GCS = local.bucket_name
+    PROJECT_DATASET_BQ = local.dataset_id
     PROJECT_NAME = local.project_id
   }
   depends_on = [null_resource.cf_code_zip]
@@ -167,7 +167,7 @@ resource "google_cloudfunctions_function" "crux_data" {
 resource "google_cloudfunctions_function_iam_member" "invoker_crux" {
   project        = var.project_id
   region         = google_cloudfunctions_function.psi.region
-  cloud_function = google_cloudfunctions_function.crux_data.name
+  cloud_function = google_cloudfunctions_function.crux.name
 
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
@@ -178,29 +178,29 @@ resource "google_cloudfunctions_function_iam_member" "invoker_crux" {
 ##################################
 # Job Scheduler Configuration
 ##################################
-resource "google_cloud_scheduler_job" "job" {
-  name             = "site-speed-dashboard-schedule"
+resource "google_cloud_scheduler_job" "scheduler_job_psi" {
+  name             = local.scheduler_name_psi
   description      = "PageSpeed Insights API HTTP Trigger"
   schedule         = "0 7 * * *"
-  time_zone        = "America/Sao_Paulo"
-  attempt_deadline = "320s"
-
+  time_zone        = local.scheduler_time_zone
+  attempt_deadline = local.scheduler_attempt_deadline
+  
   retry_config {
     retry_count = 1
   }
 
   http_target {
     http_method = "GET"
-    uri         = "https://${google_cloudfunctions_function.psi.region}-${var.project_id}.cloudfunctions.net/${local.cf_name}"
+    uri         = "https://${google_cloudfunctions_function.psi.region}-${var.project_id}.cloudfunctions.net/${local.cf_name_psi}"
   }
 }
 
-resource "google_cloud_scheduler_job" "job_crux" {
-  name             = "site-speed-dashboard-schedule-crux-data"
+resource "google_cloud_scheduler_job" "scheduler_job_crux" {
+  name             = local.scheduler_name_crux
   description      = "CrUX API HTTP Trigger"
   schedule         = "0 17 * * *"
-  time_zone        = "America/Sao_Paulo"
-  attempt_deadline = "320s"
+  time_zone        = local.scheduler_time_zone
+  attempt_deadline = local.scheduler_attempt_deadline
 
   retry_config {
     retry_count = 1
@@ -208,6 +208,6 @@ resource "google_cloud_scheduler_job" "job_crux" {
 
   http_target {
     http_method = "GET"
-    uri         = "https://${google_cloudfunctions_function.function.region}-${var.project_id}.cloudfunctions.net/crux_data"
+    uri         = "https://${google_cloudfunctions_function.psi.region}-${var.project_id}.cloudfunctions.net/${local.cf_name_crux}"
   }
 }
